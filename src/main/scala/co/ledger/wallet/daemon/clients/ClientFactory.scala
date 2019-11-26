5bc51ad8afdc50f8fd2e98e8edd538d8c43e5daf
Roman Wilhelm <roman.wilhelm@ledger.fr>
2019-11-26 15:27:23 +0100

    Move back libcore-http-client and libcore-thread-dispatcher to unbounded pools

--- src/main/scala/co/ledger/wallet/daemon/clients/ClientFactory.scala
+++ src/main/scala/co/ledger/wallet/daemon/clients/ClientFactory.scala
@@ -14,10 +14,10 @@ object ClientFactory {
 
   lazy val webSocketClient = new ScalaWebSocketClient
   lazy val httpClient = new ScalaHttpClient()(
-    ExecutionContext.fromExecutor(Executors.newFixedThreadPool(Runtime.getRuntime.availableProcessors() * 4, (r: Runnable) => new Thread(r, "libcore-http-client")))
+    ExecutionContext.fromExecutor(Executors.newCachedThreadPool((r: Runnable) => new Thread(r, "libcore-http-client")))
   )
   lazy val threadDispatcher = new ScalaThreadDispatcher(
-    ExecutionContext.fromExecutor(new ThreadPoolExecutor(Runtime.getRuntime.availableProcessors() * 4, Runtime.getRuntime.availableProcessors() * 8, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue[Runnable], (r: Runnable) => new Thread(r, "libcore-thread-dispatcher")))
+    ExecutionContext.fromExecutor(Executors.newCachedThreadPool((r: Runnable) => new Thread(r, "libcore-thread-dispatcher")))
   )
   lazy val apiClient = apiC
 }
