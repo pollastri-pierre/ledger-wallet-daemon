749e2c10cc05c8586c3776e4fa1fda474bad5bce
Roman Wilhelm <roman.wilhelm@ledger.fr>
2019-11-26 16:38:29 +0100

    CorePool is a bounded thread pool

--- src/main/scala/co/ledger/wallet/daemon/clients/ClientFactory.scala
+++ src/main/scala/co/ledger/wallet/daemon/clients/ClientFactory.scala
@@ -1,6 +1,6 @@
 package co.ledger.wallet.daemon.clients
 
-import java.util.concurrent.Executors
+import java.util.concurrent.{Executors, LinkedBlockingQueue, ThreadPoolExecutor, TimeUnit}
 
 import co.ledger.wallet.daemon.libledger_core.async.ScalaThreadDispatcher
 
@@ -16,7 +16,7 @@ object ClientFactory {
     ExecutionContext.fromExecutor(Executors.newCachedThreadPool((r: Runnable) => new Thread(r, "libcore-http-client")))
   )
   lazy val threadDispatcher = new ScalaThreadDispatcher(
-    ExecutionContext.fromExecutor(Executors.newCachedThreadPool((r: Runnable) => new Thread(r, "libcore-thread-dispatcher")))
+    ExecutionContext.fromExecutor(new ThreadPoolExecutor(Runtime.getRuntime.availableProcessors() * 4, Runtime.getRuntime.availableProcessors() * 8, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue[Runnable], (r: Runnable) => new Thread(r, "libcore-thread-dispatcher")))
   )
   lazy val apiClient = apiC
 }
