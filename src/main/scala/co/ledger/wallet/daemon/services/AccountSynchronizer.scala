93a7c69788d9b95788dbef6e83d2a33e728b9fd5
drako <dany.rakotopara@ledger.fr>
2020-10-22 09:57:21 +0200

    BACK-1020 : NRT - Avoid switch thread context JNI objects retrieved from libcore

--- src/main/scala/co/ledger/wallet/daemon/services/AccountSynchronizer.scala
+++ src/main/scala/co/ledger/wallet/daemon/services/AccountSynchronizer.scala
@@ -13,7 +13,7 @@ import co.ledger.wallet.daemon.exceptions.AccountNotFoundException
 import co.ledger.wallet.daemon.libledger_core.async.LedgerCoreExecutionContext
 import co.ledger.wallet.daemon.models.Account._
 import co.ledger.wallet.daemon.models.Wallet._
-import co.ledger.wallet.daemon.models.{AccountInfo, Pool, PoolInfo}
+import co.ledger.wallet.daemon.models.{AccountInfo, Operations, Pool, PoolInfo}
 import co.ledger.wallet.daemon.schedulers.observers.SynchronizationResult
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.twitter.concurrent.NamedPoolThreadFactory
@@ -22,6 +22,7 @@ import com.twitter.util.{Duration, ScheduledThreadPoolTimer, Timer}
 import javax.inject.{Inject, Singleton}
 
 import scala.collection.JavaConverters._
+import scala.concurrent.duration.Duration.Inf
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutorService, Future}
 import scala.util.{Failure, Success, Try}
@@ -67,20 +68,33 @@ class AccountSynchronizerManager @Inject()(daemonCache: DaemonCache, publisher: 
   def syncAccount(accountInfo: AccountInfo): Future[SynchronizationResult] = {
     Option(registeredAccounts.get(accountInfo)).fold({
       warn(s"Trying to sync an unregistered account. $accountInfo")
-      Future.failed[SynchronizationResult](AccountNotFoundException(accountInfo.accountIndex))
+      Future.failed[SynchronizationResult](
+        AccountNotFoundException(accountInfo.accountIndex)
+      )
     })(_.eventuallyStartSync())
   }
 
   def syncPool(poolInfo: PoolInfo): Future[Seq[SynchronizationResult]] =
-    daemonCache.withWalletPool(poolInfo)(pool =>
-      for {
-        wallets <- pool.wallets
-        syncResults <- wallets.toList.traverse { wallet =>
-            wallet.accounts.flatMap(_.toList.traverse(account =>
-              syncAccount(AccountInfo(account.getIndex, wallet.getName, pool.name, poolInfo.pubKey)))
+    daemonCache.withWalletPool(poolInfo)(
+      pool =>
+        for {
+          wallets <- pool.wallets
+          syncResults <- wallets.toList.traverse { wallet =>
+            wallet.accounts.flatMap(
+              _.toList.traverse(
+                account =>
+                  syncAccount(
+                    AccountInfo(
+                      account.getIndex,
+                      wallet.getName,
+                      pool.name,
+                      poolInfo.pubKey
+                    )
+                )
+              )
             )
-        }
-      } yield syncResults.flatten
+          }
+        } yield syncResults.flatten
     )
 
   def syncAllRegisteredAccounts(): Future[Seq[SynchronizationResult]] =
@@ -88,20 +102,38 @@ class AccountSynchronizerManager @Inject()(daemonCache: DaemonCache, publisher: 
       case (_, accountSynchronizer) => accountSynchronizer.eventuallyStartSync()
     }.toSeq)
 
-  def registerAccount(account: Account, wallet: Wallet, accountInfo: AccountInfo): Unit = this.synchronized {
-    registeredAccounts.computeIfAbsent(accountInfo, (i: AccountInfo) => {
-      info(s"registered account $i to account synchronizer manager")
-      new AccountSynchronizer(account, wallet, poolName = i.poolName, scheduler, publisher)
-    })
+  def registerAccount(account: Account,
+                      wallet: Wallet,
+                      accountInfo: AccountInfo): Unit = this.synchronized {
+    registeredAccounts.computeIfAbsent(
+      accountInfo,
+      (i: AccountInfo) => {
+        info(s"registered account $i to account synchronizer manager")
+        new AccountSynchronizer(
+          account,
+          wallet,
+          poolName = i.poolName,
+          scheduler,
+          publisher
+        )
+      }
+    )
   }
 
-  def unregisterAccount(accountInfo: AccountInfo): Future[Unit] = this.synchronized {
-    registeredAccounts.asScala.remove(accountInfo).fold(
-      Future.failed[Unit](AccountNotFoundException(accountInfo.accountIndex))
-    )(as => {
-      as.close(Duration.fromMinutes(3)).map(_ => info(s"AccountSynchronizer for account $accountInfo Closed"))
-    })
-  }
+  def unregisterAccount(accountInfo: AccountInfo): Future[Unit] =
+    this.synchronized {
+      registeredAccounts.asScala
+        .remove(accountInfo)
+        .fold(
+          Future
+            .failed[Unit](AccountNotFoundException(accountInfo.accountIndex))
+        )(as => {
+          as.close(Duration.fromMinutes(3))
+            .map(
+              _ => info(s"AccountSynchronizer for account $accountInfo Closed")
+            )
+        })
+    }
 
   def unregisterPool(pool: Pool, poolInfo: PoolInfo): Future[Unit] = {
     info(s"Unregister Pool $poolInfo")
@@ -181,13 +213,18 @@ class AccountSynchronizer(account: Account,
                           wallet: Wallet,
                           poolName: String,
                           scheduler: Timer,
-                          publisher: Publisher)
-                         (implicit ec: ExecutionContext) extends Logging {
+                          publisher: Publisher)(implicit ec: ExecutionContext)
+    extends Logging {
   private val walletName = wallet.getName
   // The core of the state machine, any change to it should be this.synchronised
   private var syncStatus: SyncStatus = Synced(0)
-  private val syncFuture: AtomicReference[Future[SynchronizationResult]] = new AtomicReference(
-    Future.successful(SynchronizationResult.apply(account.getIndex, walletName, poolName, syncResult = false)))
+  private val syncFuture: AtomicReference[Future[SynchronizationResult]] =
+    new AtomicReference(
+      Future.successful(
+        SynchronizationResult
+          .apply(account.getIndex, walletName, poolName, syncResult = false)
+      )
+    )
   // A counter to count the new operation event that is received.
   // Used to work with resync to indicate the resync progress
   private val opCounter: AtomicLong = new AtomicLong()
@@ -209,36 +246,68 @@ class AccountSynchronizer(account: Account,
         case EventCode.NEW_OPERATION =>
           opCounter.incrementAndGet()
           val uid = event.getPayload.getString(OP_ID_EVENT_KEY)
-          account.operation(uid, 1).foreach {
-            case Some(op) =>
-              publisher.publishOperation(op, account, wallet, poolName)
-              publisher.publishERC20Operation(op, account, wallet, poolName)
-            case _ =>
-          }
+
+          Await.result(account.operation(uid, 1), Inf).foreach(op => {
+
+              // Simple operation send
+              val view = Await.result(Operations.getView(op, wallet, account), Inf)
+              publisher.publishOperation(view, account, wallet, poolName)
+
+              // ERC20 case
+              if (account.isInstanceOfEthereumLikeAccount) {
+                val erc20Accounts = account.asEthereumLikeAccount().getERC20Accounts.asScala
+                val tx = op.asEthereumLikeOperation().getTransaction
+                val sender = tx.getSender.toEIP55
+                val receiver = tx.getReceiver.toEIP55
+
+                val senderOps = erc20Accounts
+                  .find(_.getToken.getContractAddress.equalsIgnoreCase(sender)).toSeq
+                  .flatMap(_.getOperations.asScala.filter(_.getHash.equalsIgnoreCase(tx.getHash)))
+
+                val recieverOps = erc20Accounts
+                  .find(_.getToken.getContractAddress.equalsIgnoreCase(receiver)).toSeq
+                  .flatMap(_.getOperations.asScala.filter(_.getHash.equalsIgnoreCase(tx.getHash)))
+
+                val views = Await.result(Future.sequence(
+                  (senderOps ++ recieverOps).map(erc20op => Operations.getErc20View(erc20op, op, wallet, account))
+                ), Inf)
+
+                views.foreach(publisher.publishERC20Operation(_, account, wallet, poolName))
+            }
+
+          })
+
         case EventCode.DELETED_OPERATION =>
-          val uid = event.getPayload().getString(Account.EV_DELETED_OP_UID)
-          publisher.publishDeletedOperation(uid, account, wallet, poolName)
+          val uid = event.getPayload.getString(Account.EV_DELETED_OP_UID)
+          Await.result(publisher.publishDeletedOperation(uid, account, wallet, poolName), Inf)
         case _ =>
       }
     }
   }
 
-
   account.getEventBus.subscribe(LedgerCoreExecutionContext(ec), eventReceiver)
 
   // Periodically try to trigger sync. the sync will be triggered when status is Synced
-  val periodicSyncTask = scheduler.schedule(Duration.fromSeconds(DaemonConfiguration.Synchronization.syncInterval)) {
+  val periodicSyncTask = scheduler.schedule(
+    Duration.fromSeconds(DaemonConfiguration.Synchronization.syncInterval)
+  ) {
     eventuallyStartSync()
   }
 
   // Periodically try to update the current height in resync status.
   // do nothing if the status is not Resyncing
-  private val periodicResyncStatusCheckTask = scheduler.schedule(Duration.fromSeconds(DaemonConfiguration.Synchronization.syncStatusCheckInterval)) {
+  private val periodicResyncStatusCheckTask = scheduler.schedule(
+    Duration
+      .fromSeconds(DaemonConfiguration.Synchronization.syncStatusCheckInterval)
+  ) {
     periodicUpdateStatus()
   }
   // Periodically try to resync. It's competing with periodic sync.
   // The resync will be triggered when status is Synced and there is a resync latch
-  private val periodicResyncCheckTask = scheduler.schedule(Duration.fromSeconds(DaemonConfiguration.Synchronization.resyncCheckInterval)) {
+  private val periodicResyncCheckTask = scheduler.schedule(
+    Duration
+      .fromSeconds(DaemonConfiguration.Synchronization.resyncCheckInterval)
+  ) {
     tryResyncAccount()
   }
 
@@ -265,9 +334,19 @@ class AccountSynchronizer(account: Account,
     periodicResyncStatusCheckTask.cancel()
     periodicSyncTask.cancel()
     account.getEventBus.unsubscribe(eventReceiver)
-    Future(Try(Await.result(syncFuture.get(), awaitOngoingSyncTimeout.inMilliseconds.millisecond))
-      .fold(t => s"Failed to await for end of synchronization $accountInfo, status : $syncStatus due to error : $t",
-        r => s"Successfully ended synchronization of account $accountInfo status : $syncStatus syncResult: $r"))
+    Future(
+      Try(
+        Await.result(
+          syncFuture.get(),
+          awaitOngoingSyncTimeout.inMilliseconds.millisecond
+        )
+      ).fold(
+          t =>
+            s"Failed to await for end of synchronization $accountInfo, status : $syncStatus due to error : $t",
+          r =>
+            s"Successfully ended synchronization of account $accountInfo status : $syncStatus syncResult: $r"
+        )
+    )
   }
 
   // This method is called periodically by `periodicSync` task
@@ -283,7 +362,6 @@ class AccountSynchronizer(account: Account,
     }
     syncFuture.get()
   }
-
 
   // This method is called periodically by `periodicResyncStatusCheck` task
   private def periodicUpdateStatus() = this.synchronized {
@@ -309,7 +387,9 @@ class AccountSynchronizer(account: Account,
       syncStatus match {
         case Synced(_) | FailedToSync(_) => // do resync
           // await the future to be able to sync syncStatus change
-          val targetOpCounts = Try(Await.result(account.operationCounts.map(_.values.sum), 10.seconds)).getOrElse(-1)
+          val targetOpCounts = Try(
+            Await.result(account.operationCounts.map(_.values.sum), 10.seconds)
+          ).getOrElse(-1)
           opCounter.set(0)
           syncStatus = Resyncing(targetOpCounts, 0)
           info(s"RESYNC : resyncing $accountInfo")
@@ -320,7 +400,9 @@ class AccountSynchronizer(account: Account,
           } yield syncTask
           syncFuture.set(syncTask)
         case _ => // queue the resync
-          info(s"RESYNC : the account $accountInfo is being syncing, postpone the resync")
+          info(
+            s"RESYNC : the account $accountInfo is being syncing, postpone the resync"
+          )
           resyncLatch.release()
       }
     }
@@ -328,7 +410,8 @@ class AccountSynchronizer(account: Account,
 
   private def syncAccount(): Future[SynchronizationResult] = {
     onSynchronizationStart()
-    account.sync(poolName, walletName)
+    account
+      .sync(poolName, walletName)
       .andThen {
         case Success(value) if value.syncResult =>
           this.synchronized {
@@ -337,7 +420,8 @@ class AccountSynchronizer(account: Account,
           }
         case _ =>
           this.synchronized {
-            syncStatus = FailedToSync(s"SYNC : failed to sync account $accountInfo")
+            syncStatus =
+              FailedToSync(s"SYNC : failed to sync account $accountInfo")
             onSynchronizationEnds()
           }
       }
@@ -360,13 +444,12 @@ class AccountSynchronizer(account: Account,
     }
   }
 
-
   private def accountInfo: String = {
     s"$poolName/$walletName/${account.getIndex}"
   }
 }
 
-sealed trait SyncStatus{
+sealed trait SyncStatus {
   def value: String
   def copy: SyncStatus
 }
@@ -393,14 +476,13 @@ case class FailedToSync(reason: String) extends SyncStatus {
 }
 
 /*
-  * targetHeight is the height of the most recent operation of the account before the resync.
-  * currentHeight is the height of the most recent operation of the account during resyncing.
-  * they serve as a progress indicator
-  */
-case class Resyncing(
-                      @JsonProperty("sync_status_target") targetOpCount: Long,
-                      @JsonProperty("sync_status_current") currentOpCount: Long
-                    ) extends SyncStatus {
+ * targetHeight is the height of the most recent operation of the account before the resync.
+ * currentHeight is the height of the most recent operation of the account during resyncing.
+ * they serve as a progress indicator
+ */
+case class Resyncing(@JsonProperty("sync_status_target") targetOpCount: Long,
+                     @JsonProperty("sync_status_current") currentOpCount: Long)
+    extends SyncStatus {
   @JsonProperty("value")
   def value: String = "resyncing"
 
--- src/main/scala/co/ledger/wallet/daemon/services/RabbitMQPublisher.scala
+++ src/main/scala/co/ledger/wallet/daemon/services/RabbitMQPublisher.scala
@@ -1,10 +1,10 @@
 package co.ledger.wallet.daemon.services
 
-import co.ledger.core.{Account, ERC20LikeAccount, ERC20LikeOperation, Operation, Wallet}
+import co.ledger.core.{Account, ERC20LikeAccount, Wallet}
 import co.ledger.wallet.daemon.async.MDCPropagatingExecutionContext.Implicits.global
 import co.ledger.wallet.daemon.models.Account._
 import co.ledger.wallet.daemon.models.Currency._
-import co.ledger.wallet.daemon.models.Operations
+import co.ledger.wallet.daemon.models.Operations.OperationView
 import com.rabbitmq.client.{BuiltinExchangeType, ConnectionFactory}
 import com.twitter.finatra.json.FinatraObjectMapper
 import com.twitter.inject.Logging
@@ -36,17 +36,14 @@ class RabbitMQPublisher(rabbitMQUri: String) extends Logging with Publisher {
 
   private val mapper = FinatraObjectMapper.create()
 
-  def publishOperation(op: Operation, account: Account, wallet: Wallet, poolName: String): Future[Unit] = {
-    operationPayload(op, account, wallet).map(payload =>
-      publish(poolName, getTransactionRoutingKeys(op, account, wallet.getCurrency.getName, poolName), payload)
-    )
+  def publishOperation(operationView: OperationView, account: Account, wallet: Wallet, poolName: String): Unit = {
+    val payload = mapper.writeValueAsBytes(operationView)
+    publish(poolName, getTransactionRoutingKeys(operationView, account, wallet.getCurrency.getName, poolName), payload)
   }
 
-  def publishERC20Operation(erc20Operation: ERC20LikeOperation, op: Operation, account: Account, wallet: Wallet, poolName: String): Future[Unit] = {
-    erc20OperationPayload(erc20Operation, op, account, wallet).map{ payload =>
-      val routingKey = getERC20TransactionRoutingKeys(erc20Operation, account, wallet.getCurrency.getName, poolName)
-      publish(poolName, routingKey, payload)
-    }
+  def publishERC20Operation(operationView: OperationView, account: Account, wallet: Wallet, poolName: String): Unit = {
+    val payload = mapper.writeValueAsBytes(operationView)
+    publish(poolName, getERC20TransactionRoutingKeys(operationView, account, wallet.getCurrency.getName, poolName), payload)
   }
 
   def publishAccount(account: Account, wallet: Wallet, poolName: String, syncStatus: SyncStatus): Future[Unit] = {
@@ -110,36 +107,24 @@ class RabbitMQPublisher(rabbitMQUri: String) extends Logging with Publisher {
     )
   }
 
-  private def erc20OperationPayload(erc20Operation: ERC20LikeOperation, operation: Operation, account: Account, wallet: Wallet): Future[Array[Byte]] = {
-    Operations.getErc20View(erc20Operation, operation, wallet, account).map{
-      mapper.writeValueAsBytes(_)
-    }
-  }
-
-  private def operationPayload(op: Operation, account: Account, wallet: Wallet): Future[Array[Byte]] = {
-    Operations.getView(op, wallet, account).map {
-      mapper.writeValueAsBytes(_)
-    }
-  }
-
-  private def getERC20TransactionRoutingKeys(erc20Op: ERC20LikeOperation, account: Account, currencyName: String, poolName: String): List[String] = {
+  private def getERC20TransactionRoutingKeys(operationView: OperationView, account: Account, currencyName: String, poolName: String): List[String] = {
     List(
       "transactions",
       poolName,
       currencyName,
       account.getIndex.toString,
       "erc20",
-      erc20Op.getOperationType.toString.toLowerCase()
+      operationView.opType.toString.toLowerCase()
     )
   }
 
-  private def getTransactionRoutingKeys(op: Operation, account: Account, currencyName: String, poolName: String): List[String] = {
+  private def getTransactionRoutingKeys(operationView: OperationView, account: Account, currencyName: String, poolName: String): List[String] = {
     List(
       "transactions",
       poolName,
       currencyName,
       account.getIndex.toString,
-      op.getOperationType.toString.toLowerCase
+      operationView.opType.toString.toLowerCase
     )
   }
 }
