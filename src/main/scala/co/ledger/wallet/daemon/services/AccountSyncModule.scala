1ac6f94f99441ee765b5b7e59804381dab5264a9
Miguel Basire <miguel.basire-ext@ledger.fr>
2020-10-28 17:01:32 +0100

    BACK-1052 Inject dependencies inside AccountSynchronizerManager/AccountSynchronizer * In order to test the AccountSynchronizerManager without testing AccountSynchronizer the AccountSynchronizer instantiation is delegated to a factory. * Operations publishing has been extracted from the AccountSynchronizer.

---
+++ src/main/scala/co/ledger/wallet/daemon/services/AccountSyncModule.scala
@@ -0,0 +1,27 @@
+package co.ledger.wallet.daemon.services
+
+import co.ledger.core.{Account, Wallet}
+import com.google.inject.{AbstractModule, Provides, Singleton}
+import com.twitter.concurrent.NamedPoolThreadFactory
+import com.twitter.util.{ScheduledThreadPoolTimer, Timer}
+
+import scala.concurrent.ExecutionContext
+
+
+object AccountSyncModule extends AbstractModule {
+  type PoolName = String
+  type AccountSynchronizerFactory = (Account, Wallet, PoolName, ExecutionContext) => AccountSynchronizer
+
+  @Provides def providesTimer: Timer = new ScheduledThreadPoolTimer(
+    poolSize = 1,
+    threadFactory = new NamedPoolThreadFactory("AccountSynchronizer-Scheduler")
+  )
+
+  @Provides @Singleton
+  def providesAccountSynchronizer(publisher: Publisher, scheduler : Timer) : AccountSynchronizerFactory = {
+    (a, w, p, ec) => new AccountSynchronizer(a, w, p, scheduler, publisher)(ec)
+  }
+
+
+  override def configure(): Unit = ()
+}
